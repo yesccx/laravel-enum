c154f57c139e7ca06aad70729ea74e2bd5d2dd11
yesccx <>
2023-06-21 10:18:42 +0800

    style: 文案调整

--- src/Supports/Message.php
+++ src/Supports/Message.php
@@ -12,7 +12,7 @@ class Message
     /**
      * @param string $column 枚举字段名
      * @param string|null $message 枚举值说明
-     * @param mixed $message 枚举值
+     * @param mixed $value 枚举值
      */
     public function __construct(
         public string $column,
